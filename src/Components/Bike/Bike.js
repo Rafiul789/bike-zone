525635638249641d590d1c59b4a772e90fde84c5
Rafiul789 <89851417+Rafiul789@users.noreply.github.com>
2022-03-26 13:10:48 +0600

    button and icon added

--- src/Components/Bike/Bike.js
+++ src/Components/Bike/Bike.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Bike.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Bike = ({bike}) => {
     const{name,seller,price,ratings,img}=bike;
@@ -11,8 +13,8 @@ const Bike = ({bike}) => {
             <div  className="description">  <h3>Name:  {name}</h3>
             <p>Seller: {seller}</p>
             <p>Price: {price}</p>
-            <p>Ratings: {ratings}</p>  </div>
-           
+            <p>Ratings: {ratings}</p>    <button className="addCart-btn" >  <p>Add to Cart  <FontAwesomeIcon icon={faShoppingCart} ></FontAwesomeIcon></p>   </button>  </div>
+         
            
         </div>
     );
